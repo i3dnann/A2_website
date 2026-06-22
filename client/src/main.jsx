048d0e5d97705631ffa335bb7492153d4cbc55c9
ADNAN <adnanneaimi@gmail.com>
2026-06-22 05:08:06 +0300

    Import map tile styles globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n";
 import "./index.css";
+import "./map.css";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 
