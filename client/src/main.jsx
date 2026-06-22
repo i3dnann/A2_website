b42f499ae98a92f3eb1a2c7c77a02566596407c5
ADNAN <adnanneaimi@gmail.com>
2026-06-22 05:31:04 +0300

    Normalize GTA map tile source

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,18 @@ import "./map.css";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 
+function normalizeGtaMapTiles() {
+  const fix = () => {
+    document.querySelectorAll('img[src*="/atlas/"]').forEach((img) => {
+      img.src = img.src.replace("/atlas/", "/mainmap/");
+    });
+  };
+  fix();
+  new MutationObserver(fix).observe(document.documentElement, { childList: true, subtree: true });
+}
+
+normalizeGtaMapTiles();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
