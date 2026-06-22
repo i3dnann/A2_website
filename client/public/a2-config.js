eec97e0706504770059440a2e5a94a37767874da
ADNAN <adnanneaimi@gmail.com>
2026-06-23 00:33:11 +0300

    Update runtime config variable name

--- client/public/a2-config.js
+++ client/public/a2-config.js
@@ -1,3 +1,3 @@
 // Optional runtime override. Netlify builds should normally use VITE_API_BASE_URL.
 // If a build was deployed without the env var, edit this value and redeploy.
-window.__A2_API_BASE_URL__ = "";
+window.__GOTHAM_API_BASE_URL__ = "";
